dcbcb1e5741005e068aa464d3848923952f3fa50
Fedor Borshev <f@f213.in>
2017-01-08 14:25:13 +0300

    Stripping and restoring HTML tags

--- richtypo.py
+++ richtypo.py
@@ -4,30 +4,49 @@ import re
 class Richtypo(object):
     text = ''
 
-    save_tags_re = [
-        re.compile(r'<([^\>]+)>'),
+    bypass_tags = [
+        'script',
+        'pre',
+        'code'
     ]
 
-    def __init__(self, text=''):
+    def __init__(self, text, bypass_tags=bypass_tags):
         self.text = text
+
+        self.save_tags_re = []
+        for tag in bypass_tags:
+            self.save_tags_re.append(self._tag_bypass_regex(tag))
+
+        self.save_tags_re.append(re.compile(r'<([^>]+)>'))  # generic regex to strip all <tags>
+
         self.saved_tags = []
 
+    def _tag_bypass_regex(self, tag):
+        return re.compile(r'<(%s[^>]*>.+</%s)>' % (tag, tag), flags=re.MULTILINE | re.DOTALL)
+
     def strip_tags(self):
+        """
+        Replace all tags with ~<tag_num>~
+        """
         replacement_count = 0
 
         def repl(m):
-            nonlocal replacement_count  # TODO: migrate to py2
+            nonlocal replacement_count  # TODO: make py2-compatible
 
             tag = m.group(1)
             self.saved_tags.append(tag)
+            print(self.saved_tags)
 
             replacement_count += 1
-            return '<%d>' % replacement_count
+            return '~%d~' % replacement_count
 
-        for re_num, regex in enumerate(self.save_tags_re):
+        for regex in self.save_tags_re:
             self.text = regex.sub(repl, self.text)
 
     def restore_tags(self):
-        restore_tags = re.compile(r'<(\d+)>')
+        """
+        Restore tags, stripped by strip_tags
+        """
+        restore_tags = re.compile(r'~(\d+)~')
 
         self.text = restore_tags.sub(lambda f: '<%s>' % self.saved_tags.pop(0), self.text)
--- tests/test_tags.py
+++ tests/test_tags.py
@@ -3,16 +3,16 @@ import pytest
 
 
 @pytest.mark.parametrize("input, expected", [
-    ('<h1>test</h1>', '<1>test<2>'),
-    ('<h1>тест</h1>', '<1>тест<2>'),
-    (r'<h1>test</', r'<1>test</'),
+    ('<h1>test</h1>', '~1~test~2~'),
+    ('<h1>тест</h1>', '~1~тест~2~'),
+    (r'<h1>test</', r'~1~test</'),
     ('тест', 'тест'),
-    ('<!-- h1 -->test</-->', '<1>test<2>'),
-    ('<H1>test</H1>', '<1>test<2>'),
-    ('<h1>test<h1>', '<1>test<2>'),
-    ('<h1><1>test</h1>', '<1><2>test<3>'),
+    ('<!-- h1 -->test</-->', '~1~test~2~'),
+    ('<H1>test</H1>', '~1~test~2~'),
+    ('<h1>test<h1>', '~1~test~2~'),
+    ('<h1><1>test</h1>', '~1~~2~test~3~'),
 ])
-def test_strip_tags(input, expected):
+def test_strip_tags_simple(input, expected):
     r = Richtypo(input)
     r.strip_tags()
     assert r.text == expected
@@ -34,3 +34,74 @@ def test_restore_tags(input):
     r.strip_tags()
     r.restore_tags()
     assert r.text == input
+
+
+@pytest.mark.parametrize("input, expected", [
+    (
+        '<script type="text/javascript">script_code()</script><h1>test</h1>',
+        '~1~~2~test~3~'
+    ),
+    (
+        """
+        <script type="text/javascript">
+            console.log("<script type="text/javascript">alert('test');</script>");
+        </script>
+        <h1>test<h2>
+        """,
+
+        """
+        ~1~
+        ~2~test~3~
+        """
+    ),
+    (
+        """
+        <script type="text/javascript">
+            scriptcode()
+        </script>
+        <h1>test</h1>
+        """,
+
+        """
+        ~1~
+        ~2~test~3~
+        """
+    ),
+])
+def test_strip_tags_in_scripts(input, expected):
+    r = Richtypo(input)
+    r.strip_tags()
+    assert r.text == expected
+
+
+@pytest.mark.parametrize("input", [
+    '<script type="text/javascript">scriptcode()</script><h1>test</h1>',
+
+    """
+    <script type="text/javascript">
+        console.log("<script type="text/javascript">alert('test');</script>");
+    </script><h1>test<h2>
+    """,
+
+    """
+    <script type="text/javascript">
+            script_code()
+    </script>
+    <h1>test</h1>
+    """,
+])
+def test_restore_tags_in_scripts(input):
+    r = Richtypo(input)
+    r.strip_tags()
+    r.restore_tags()
+    assert r.text == input
+
+
+@pytest.mark.parametrize("input, expected, bypass_tags", [
+    ('<pre>test</pre>', '~1~test~2~', []),
+    ('<test>test</test>', '~1~', ['test']),
+])
+def test_bypass_tags(input, expected, bypass_tags):
+    r = Richtypo(input, bypass_tags=bypass_tags)
+    r.strip_tags()
+    assert r.text == expected
