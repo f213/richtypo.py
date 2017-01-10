b8e43f4bf3baecffa54808cf344e9a3b78aa692d
Fedor Borshev <f@f213.in>
2017-01-10 22:42:35 +0300

    Restored py2 checking of rule specs

--- tests/test_rule_specs.py
+++ tests/test_rule_specs.py
@@ -1,15 +1,20 @@
-import pytest
+# -*- coding: utf-8
 import six
 
 from richtypo.rules import SPECIAL_CHARACTERS_MAP
 
 
 def _replace_unicode_back_to_special_characters(unicode_input):
-    reverse_map = dict((v, k) for k, v in SPECIAL_CHARACTERS_MAP.items())
-    return unicode_input.translate(str.maketrans(reverse_map))
+    """
+    Translate the output of richtypo to the language used in rule files,
+    e.g. translate ` ` (non-breakable space) to `_`
+    """
+    for k, v in six.iteritems(SPECIAL_CHARACTERS_MAP):
+        unicode_input = unicode_input.replace(v, k)
+
+    return unicode_input
 
 
-@pytest.mark.skipif(six.PY2, reason="I'm too lazy to make this work on python2")
 def test_rule_specs(rule_name, rule):
     for spec in rule.specs:
         (input, expected) = spec.split('|')
