c71a7158ad468d5a5816662b78cded19915f26a8
Fedor Borshev <f@f213.in>
2017-01-11 16:28:37 +0300

    Packaged that stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='richtypo',
     version='0.1',
-    # py_modules=['foo'],
+    description="The only web-typograf thet doesn't make your text a mess. Russian and Eglish supported.",
+    url="https://github.com/f213/richtypo.py",
+    author="Fedor Borshev",
+    author_email="f@f213.in",
+    license="MIT",
+    packages=find_packages(),
+    setup_requires=['setuptools-git'],
+    install_requires=[
+        'six',
+        'PyYAML',
+    ],
+    include_package_data=True,
+    zip_safe=False,
 )
